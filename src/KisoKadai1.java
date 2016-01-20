e90afa72beae46afe7de555e5a2995319341eb9b
'Saori <'sa.moko621@gmail.com'>
2016-01-20 16:35:01 +0900

    KisoKadai1-SaoriMaekawa

---
+++ src/KisoKadai1.java
@@ -0,0 +1,30 @@
+ /**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class KisoKadai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		kuku(9, 9);
+	}
+
+		public static void kuku(int x, int y){
+			for (int i = 1; i < 10; i++){
+			  for (int j = 1; j < 10; j++){
+				System.out.print(i * j);
+				System.out.print(" ");
+			}
+			  System.out.println();
+			}
+		}
+
+
+
+}
