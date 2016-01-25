5e92ecedba4407af30e912fb60adbfa63ea8cc0b
'Saori <'sa.moko621@gmail.com'>
2016-01-25 17:37:53 +0900

    KisoKadai2/SaoriMaekawa

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -17,10 +17,7 @@ public class KisoKadai1 {
 
 			for (int i = 1; i <= x; i++){
 			  for (int j = 1; j <= y; j++){
-
-				System.out.print(" ");
-				System.out.print(i * j);
-				if(i*j < 10) {System.out.print(" ");}
+				  System.out.print((i*j)>=10 ? " "+(i*j) : "  "+(i*j));
 
 			}
 			  System.out.println();
