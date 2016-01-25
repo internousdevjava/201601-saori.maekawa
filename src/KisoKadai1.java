e3b728bfeba76eb0b801c65eefb2185b6fe039ed
'Saori <'sa.moko621@gmail.com'>
2016-01-25 14:32:10 +0900

    KisoKadai1/SaoriMaekawa

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,18 +1,23 @@
-
-
 /*
  * 掛け算九九表の作成
- *
+ *作成者：前川沙織  作成日：2016年1月
  */
 public class KisoKadai1 {
 
 		public static void main(String[] args) {
-		kuku(9, 9);
+			System.out.println("Xの数字を入力してください");
+			int x = new java.util.Scanner(System.in).nextInt();
+			System.out.println("yの数字を入力してください");
+			int y = new java.util.Scanner(System.in).nextInt();
+
+			kuku(x, y);
 	}
 
 		public static void kuku(int x, int y){
-			for (int i = 1; i < 10; i++){
-			  for (int j = 1; j < 10; j++){
+
+			for (int i = 1; i <= x; i++){
+			  for (int j = 1; j <= y; j++){
+
 				System.out.print(i * j);
 				System.out.print(" ");
 			}
