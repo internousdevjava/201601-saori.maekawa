b2115192bd5247295221cc840e7daf33e4433c22
'Saori <'sa.moko621@gmail.com'>
2016-01-25 16:23:41 +0900

    KisoKdai3/SaoriMaekawa

--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -14,7 +14,7 @@ public class KisoKadai3 {
 	public static void main(String[] args)throws IOException {
 		while(true){
 			System.out.println("何をしますか？\n");
-			System.out.println("--メニュー--\n1:フォルダ作成 2:ファイルの読み込み 3:ファイルの書き込み 4:終了");
+			System.out.println("--メニュー--\n1:フォルダ作成 2:ファイル作成 3:ファイルの読み込み 4:ファイルの書き込み 5:終了");
 			@SuppressWarnings("resource")
 			int i = new java.util.Scanner(System.in).nextInt();
 
@@ -29,14 +29,34 @@ public class KisoKadai3 {
 
 					if (newfile.mkdirs()){
 						System.out.println("フォルダをの作成に成功しました");
+						System.out.println("cドライブ直下に保存されました");
 					} else {
 						System.out.println("フォルダをの作成に失敗しました\n既に存在します");
 					}
 
 				}
 
+				if(i == 2){
+					System.out.println("ファイルを作成します");
+					System.out.println("ファイル名を絶対パスで入力してください");
+					@SuppressWarnings("resource")
+					String input4 = new java.util.Scanner(System.in).nextLine();
+					File file4 = new File(input4);
 
-				if (i == 2){
+				    try{
+				      if (file4.createNewFile()){
+				        System.out.println("ファイルの作成に成功しました");
+				      }else{
+				        System.out.println("ファイルの作成に失敗しました");
+				      }
+				    }catch(IOException e){
+				      System.out.println(e);
+				    }
+
+				}
+
+                //ファイルの読み込み
+				if (i == 3){
 					System.out.println("ファイル名を絶対パスで入力してください");
 					@SuppressWarnings("resource")
 					String input2 = new java.util.Scanner(System.in).nextLine();
@@ -57,14 +77,20 @@ public class KisoKadai3 {
 						}
 				}
 
-				if (i == 3){
-					System.out.println("ファイル名を絶対パスで入力してください");
-					String input3 = new java.util.Scanner(System.in).nextLine();
+				if (i == 4){
+					System.out.println("モードの設定\n1:上書き 2:追記");
+					@SuppressWarnings("resource")
+					int mode = new java.util.Scanner(System.in).nextInt();
 
-					 File file3 = new File( input3 );
-					if (file3.exists()){
-						try{
-							 System.out.println("入力してください");
+					// 上書き
+					if(mode ==1){
+						System.out.println("ファイル名を絶対パスで入力してください");
+						@SuppressWarnings("resource")
+						String input3 = new java.util.Scanner(System.in).nextLine();
+						File file3 = new File( input3 );
+						if (file3.exists()){
+							try{
+								System.out.println("入力してください");
 					    	    @SuppressWarnings("resource")
 								String overwrite = new java.util.Scanner(System.in).nextLine();
 					    	    FileWriter filewriter = new FileWriter(file3);
@@ -80,17 +106,48 @@ public class KisoKadai3 {
 						      System.out.println(e);
 						    }
 
-			      }else{
-					   System.out.println("認識できません");
-					   return ;
+						}else{
+							System.out.println("認識できません");
+						return ;
 				    	}
 
+					}
 
+					//追記
+					if(mode == 2){
+						System.out.println("ファイル名を絶対パスで入力してください");
+						@SuppressWarnings("resource")
+						String input3 = new java.util.Scanner(System.in).nextLine();
+						File file3 = new File( input3 );
+						if (file3.exists()){
+							try{
+								System.out.println("入力してください");
+					    	    @SuppressWarnings("resource")
+								String postscript = new java.util.Scanner(System.in).nextLine();
+					    	    FileWriter filewriter = new FileWriter(file3,true);
+
+					    	    filewriter.write( "\r\n" + postscript );
+					    	    System.out.println( postscript + "と、上書きしました");
+
+					    	    filewriter.close();
+
+
+						    }catch(IOException e){
+						      System.out.println("ファイルに書き込めません");
+						      System.out.println(e);
+						    }
+
+						}else{
+							System.out.println("認識できません");
+						return ;
+				    	}
+
+					}
 
 
 				}
 
-				if (i == 4){
+				if (i == 5){
 
 					break;
 
