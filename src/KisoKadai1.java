c6dac86bdaaca9849d2eeeca061e5b338f0293f8
'Saori <'sa.moko621@gmail.com'>
2016-01-26 14:58:09 +0900

    KisoKadai1/SaoriMaekawa

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /*
  * 掛け算九九表の作成
  *作成者：前川沙織  作成日：2016/jan
@@ -5,13 +8,54 @@
 public class KisoKadai1 {
 
 		public static void main(String[] args) {
-			System.out.println("xに1-9の中で数字を入力してください");
-			int x = new java.util.Scanner(System.in).nextInt();
-			System.out.println("yに1-9の中で数字を入力してください");
-			int y = new java.util.Scanner(System.in).nextInt();
 
-			kuku(x, y);
+			while(true){
+				int x;
+				int y;
+				int flag=0;
+				System.out.println("xに1-9の中で数字を入力してください");
+				InputStreamReader isr = new InputStreamReader(System.in);
+                BufferedReader br = new BufferedReader(isr);
+                try{
+                        String buf = br.readLine();
+                        x = Integer.parseInt(buf);
+                }catch(Exception e){
+                	flag=1;
+                	x=10;
+
+                        System.out.println("数字を入力してください");
+                }
+				System.out.println("yに1-9の中で数字を入力してください");
+				InputStreamReader isr1 = new InputStreamReader(System.in);
+                BufferedReader br1 = new BufferedReader(isr1);
+                try{
+                        String buf = br1.readLine();
+                        y = Integer.parseInt(buf);
+                }catch(Exception e){
+
+                	flag=1;
+                	y=10;
+                        System.out.println("数字を入力してください");
+                }
+
+				if(x >= 10 || y >=10){
+					System.out.println("1-9の中で数字を入力してください");
+				}
+
+				else{
+					if(flag!=1){
+					kuku(x, y);
+					break;
+					}
+				}
+
+
+
+
+
+			}
 	}
+
 
 		public static void kuku(int x, int y){
 
